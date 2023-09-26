fdeb3ae01cb2f01c808c887cd453ab69f2405bd0
Sennaf0x <Sennafo@hotmail.com>
2023-09-26 02:55:27 -0400

    Construindo a página de login

--- Turismo-Angular/src/app/app-routing.module.ts
+++ Turismo-Angular/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path:'Login', component: HomeComponent},
+  { path:'Login', component: LoginComponent},
   { path:'home', component: HomeComponent},
   { path:'aboutus', component: HomeComponent},
   { path:'balnearios', component: HomeComponent},
--- Turismo-Angular/src/app/app.module.ts
+++ Turismo-Angular/src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatToolbarModule, MatButtonModule, MatIconModule } from '@angular/material';
-
+import { MatToolbarModule, MatButtonModule, MatIconModule, MatFormFieldModule } from '@angular/material';
+import { MatInputModule } from '@angular/material/input';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './home/navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -20,7 +22,9 @@ import { NavbarComponent } from './home/navbar/navbar.component';
     MatToolbarModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
