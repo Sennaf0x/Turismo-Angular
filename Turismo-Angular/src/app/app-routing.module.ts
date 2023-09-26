6dd25e8d39483639142d9eeebb7499d1282265e8
Sennaf0x <Sennafo@hotmail.com>
2023-09-26 00:22:49 -0400

    Criando barra de navegação com angular material

--- Turismo-Angular/src/app/app-routing.module.ts
+++ Turismo-Angular/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'Login', component: HomeComponent},
+  { path:'home', component: HomeComponent},
+  { path:'aboutus', component: HomeComponent},
+  { path:'balnearios', component: HomeComponent},
+  { path:'gencias', component: HomeComponent},
+  { path:'turismo', component: HomeComponent},
+  { path:'autonomo', component: HomeComponent},
+  { path:'contato', component: HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
